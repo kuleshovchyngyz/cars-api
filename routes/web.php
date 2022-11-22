91eb69205e7a487a6080d4d0ddf22f23345c6937
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-11-23 02:25:35 +0600

    adding vendor

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    $types = \Illuminate\Support\Facades\Cache::remember('types',33600,function (){
-       return    \App\Models\CarType::all();
-    });
+//    $types = \Illuminate\Support\Facades\Cache::remember('types',33600,function (){
+//       return    \App\Models\CarType::all();
+//    });
     return view('welcome',compact('types'));
 });
 Route::get('/get', function () {
 
-    $types = \Illuminate\Support\Facades\Cache::remember('types',33600,function (){
-        return   11;
-    });
-   dd($types);
+//    $types = \Illuminate\Support\Facades\Cache::remember('types',33600,function (){
+//        return   11;
+//    });
+//   dd($types);
 //    return view('welcome');
 });
