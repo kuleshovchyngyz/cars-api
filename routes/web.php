b2aeba16f5b86b8888ece5eb9a5a694ffd48a697
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-11-23 02:42:04 +0600

    fixing web

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
-//    $types = \Illuminate\Support\Facades\Cache::remember('types',33600,function (){
-//       return    \App\Models\CarType::all();
-//    });
     return view('welcome');
 });
-Route::get('/get', function () {
-
-//    $types = \Illuminate\Support\Facades\Cache::remember('types',33600,function (){
-//        return   11;
-//    });
-//   dd($types);
-//    return view('welcome');
-});
+//Route::get('/get', function () {
+//
+////    $types = \Illuminate\Support\Facades\Cache::remember('types',33600,function (){
+////        return   11;
+////    });
+////   dd($types);
+////    return view('welcome');
+//});
