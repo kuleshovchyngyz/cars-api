e5d3e0fc50a8c9d789bc0300df262177c1f21926
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-11-23 15:57:27 +0600

    adding methods

--- routes/api.php
+++ routes/api.php
@@ -19,18 +19,18 @@ Route::post('auth/login', [\App\Http\Controllers\AuthController::class, 'login']
 Route::group(['prefix'=>'v1'],function () {
 
     Route::get('types', [CarController::class, 'carTypes']);
-//Route::get('types/{carType}',[CarController::class,'carType']);
+Route::get('types/{carType}',[CarController::class,'carType']);
     Route::get('types/{carType}/marks', [CarController::class, 'carMarks']);
-//Route::get('marks/{carMark}',[CarController::class,'carMark']);
+Route::get('marks/{carMark}',[CarController::class,'carMark']);
     Route::get('marks/{carMark}/models', [CarController::class, 'carModels']);
-//Route::get('models/{carModel}',[CarController::class,'carModel']);
+Route::get('models/{carModel}',[CarController::class,'carModel']);
     Route::get('models/{carModel}/years', [CarController::class, 'years']);
     Route::get('models/{carModel}/years/{year}/generations', [CarController::class, 'generations']);
-//Route::get('years/{year}/generations/{carGeneration}',[CarController::class,'generation']);
+Route::get('years/{year}/generations/{carGeneration}',[CarController::class,'generation']);
     Route::get('generations/{carGeneration}/series', [CarController::class, 'series']);
-//Route::get('series/{carSeries}',[CarController::class,'serie']);
+Route::get('series/{carSeries}',[CarController::class,'serie']);
     Route::get('model/{carModel}/series/{carSeries}/{year}', [CarController::class, 'modifications']);
-//Route::get('modifications/{carModification}',[CarController::class,'modification']);
+Route::get('modifications/{carModification}',[CarController::class,'modification']);
     Route::get('modifications/{carModification}/engines', [CarController::class, 'engines']);
     Route::get('modifications/{carModification}/transmissions', [CarController::class, 'transmissions']);
     Route::get('modifications/{carModification}/gears', [CarController::class, 'gears']);
